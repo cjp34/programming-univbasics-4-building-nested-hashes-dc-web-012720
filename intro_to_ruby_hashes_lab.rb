cff32aaa640074c7a9f37130420e7c3ab6a859fe
Carl Parm <carlparm@gmail.com>
2020-01-16 17:42:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,14 +16,7 @@ def monopoly_with_third_tier
   monopoly = {
     :railroads => {
       :pieces => 4,
-      :rent_in_dollars => {
-        :key1 => "",
-        :key2 => "",
-        :key3 => "",
-        :key4 => ""
-      },
-      :names => "4"
-    }
+      :rent_in_dollars => 4
   }
 end
 
